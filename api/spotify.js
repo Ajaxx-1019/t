00ad00641faddc1b0956c0c182f8b3fd314fa10b
Ajaxx-1019 <muhammadrajaa1911@gmail.com>
2026-07-25 17:16:17 +0700

    Update spotify.js

--- api/spotify.js
+++ api/spotify.js
@@ -1,1 +1,29 @@
-w
+export default async function handler(req, res) {
+  try {
+    const q = req.query.q || "";
+
+    if (!q) {
+      return res.status(400).json({
+        status: false,
+        message: "Query kosong."
+      });
+    }
+
+    const response = await fetch(
+      `https://api.nexadev.my.id/api/spotifyplay?q=${encodeURIComponent(q)}`
+    );
+
+    const data = await response.json();
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET");
+
+    return res.status(200).json(data);
+
+  } catch (err) {
+    return res.status(500).json({
+      status: false,
+      message: err.message
+    });
+  }
+}
